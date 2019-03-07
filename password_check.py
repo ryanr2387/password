7e734c6568e4132fa70f89584cf77f3c2d1ee72e
Ryan Rayes <rrayes@student.waubonsee.edu>
2019-03-07 11:44:07 -0600

    rename file pass.py to password_check.py

--- password_check.py
+++ password_check.py
@@ -1,8 +1,12 @@
 PASSWORD = 'Waubonsee'
 user_password = input('Enter user password: ')
 
+count = 1
 while PASSWORD != user_password:
     print('INCORRECT PASSWORD')
+    print(count)
     user_password = input('Enter user password: ')
+    count = count + 1
+    #print(count)
 
 print('CORRECT')
